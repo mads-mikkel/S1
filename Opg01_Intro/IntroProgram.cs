a71b9bde3bf356b7f6fb22bb2afa56354474455e
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-04-13 15:34:54 +0200

    Add project files.

---
+++ Opg01_Intro/IntroProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+namespace Opg01_Intro
+{
+    class IntroProgram
+    {
+        [SupportedOSPlatform("windows")]
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WindowHeight = 10;
+            Console.WindowWidth = 30;
+            Console.WriteLine("Test");
+        }
+    }
+}
+
---
+++ Opg02_DataTypes/DataTypesProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opg02_DataTypes
+{
+    class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Noget med datatyper");
+        }
+    }
+}
