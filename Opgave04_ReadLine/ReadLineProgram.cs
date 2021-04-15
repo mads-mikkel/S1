6313d5abd19b0405a3fe233aaddd600f1bef22c5
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-04-15 10:57:56 +0200

    vejl. løsn. til opg. 4.4.

--- Opgave04_ReadLine/ReadLineProgram.cs
+++ Opgave04_ReadLine/ReadLineProgram.cs
@@ -25,16 +25,39 @@ namespace Opgave04_ReadLine
             Console.WriteLine(output);
 
             // 4.3 Indlæs et tal fra konsollen
-            // *** LÆRER NOTER: I opgaven står der følgende kodelinje:
-            //     int age = int.Parse( Console.ReadLine() ); 
-            // Didaktisk kan det være fordelagtigt at introducere eleven for Convert klassen i stedet for Parse metoder, samt at træne variable ved at gemme inputtet som en streng, inden konversionen foretages:
+            /* *** LÆRER NOTER: I opgaven står der følgende kodelinje:
+                    int age = int.Parse( Console.ReadLine() ); 
+                Didaktisk kan det være fordelagtigt at introducere eleven for Convert klassen i stedet for Parse metoder, samt at træne variable ved at gemme inputtet som en streng,
+                inden konversionen foretages: */
             Console.WriteLine("OPG. 4.3 Indlæs et tal fra konsollen");
-            Console.WriteLine("Skriv et tal:");
-            string numberInput = Console.ReadLine();    // Gem altid input i en variabel.
-            int number = Convert.ToInt32(numberInput);  // Brug Convert klassen og ikke Parse, da det semantisk er mere læseligt. ToInt32 betyder bare int.
-            Console.WriteLine(number);                  // Vi behøver ikke at gen-konvertere til en string, da WriteLine kan modtage hvad-som-helst og udskrive det. Så vi sender bare number variablen ind i WriteLine metoden, og så sørger den for resten.
-            // *** LÆRER NOTE: Når man indtaster noget andet end et tal, får man en exception - altså en program undtagelse, bedre kendt som et crash
+            Console.WriteLine("Skriv et helt tal:");
+            string integerNumberInput = Console.ReadLine();    // Gem altid input i en variabel.
+            int number = Convert.ToInt32(integerNumberInput);  // Brug Convert klassen og ikke Parse, da det semantisk er mere læseligt. ToInt32 betyder bare int (alias type).
+            Console.WriteLine(number);                  // Vi behøver ikke at gen-konvertere til en string, da WriteLine kan modtage hvad-som-helst og udskrive det. Så vi sender bare number variablen ind i WriteLine metoden, og så sørger den for resten. Jeg oplever jævnligt elever der gemmer i en string variabel inden udskrivning, hvilket er unødvendigt.
+            // *** LÆRER NOTE: Når man indtaster noget andet end et tal, får man en exception - altså en program undtagelse, bedre kendt som et crash.
+            // Kommatal:
+            Console.WriteLine("Skriv et kommatal:");
+            string decimalNumberInput = Console.ReadLine();
+            double decimalNumber = Convert.ToDouble(decimalNumberInput);
+            Console.WriteLine(decimalNumber);
+            /* *** LÆRER NOTE: På dansk anvender vi komma som decimaltalsseparator, men det på engelsk anvendes punktum. 
+             * Anvender man punktum som decimaltalsseparator i input (f.eks. 2.56), konverteres input til double værdien 256. 
+             * Anvendes komma, får man som forventet 2,56. */
 
+            // 4.4 Ekstra opgave: arealudregning
+            /* *** LÆRER NOTE: Dette er en ekstraopgave fordi der skal anvendes en aritmetisk operator * (multiplikation), men det er først i Opgave 05 at eleven skal introduceres til det. Denne opgave er derfor til de elever der er særligt videnbegærlige.*/
+            Console.WriteLine("Længde (m):");
+            string lengthInput = Console.ReadLine();
+            double length = Convert.ToDouble(lengthInput);  // Brugeren bliver spurgt efter længden i meter, som jo kan være et decimaltal, derfor konverteres input til double.
+            Console.WriteLine("Bredde (m):");
+            string widthInput = Console.ReadLine();
+            double width = Convert.ToDouble(widthInput);
+            double area = length * width;   // Resultatet af multiplikationen skal også gemmes i en double, da to decimaltal ganget sammen også vil give et decimaltal.
+            Console.WriteLine($"Areal: {area} m²");     // to tallet er unicode karakter U+00B2. Kopieret ind i VS fra et andet program eller browser, da det ikke kan genskabes via tastaturet.
+
+            // 4.5 Ekstra opgave: Console.WriteLine()
+            /* *** LÆRER NOTE: Denne opgave er mest for at få tiden til gå, hvis der er stor forskel på eleverns arbejdstempo. 
+             Opgaven kan enten løses ved at lave 6 Console.WriteLine eller mere udfordrende, ved at lave én string variabel og så outputte den i en Console.WriteLine.*/
         }
     }
 }
