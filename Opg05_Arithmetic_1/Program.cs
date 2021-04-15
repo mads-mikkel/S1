354d9e1545ab4b2ab89086e668dc63b6f4cb3a45
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-04-15 13:04:52 +0200

    Vejl- løsn. til opg. 5.1

---
+++ Opg05_Arithmetic_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Opg05_Arithmetic_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lommeregner v1");
+
+            // Indhent første tal fra brugeren og konverter:
+            Console.WriteLine("Indtast første tal:");
+            string firstNumberInput = Console.ReadLine();
+            int firstNumber = Convert.ToInt32(firstNumberInput);
+
+            // Indhent første tal fra brugeren og konverter:
+            Console.WriteLine("Indtast andet tal:");
+            string secoondNumberInput = Console.ReadLine();
+            int secondNumber = Convert.ToInt32(secoondNumberInput);
+
+            // Foretag den aritmetiske operation, og gem i en variabel:
+            int result = firstNumber + secondNumber;
+
+            // Udskriv resultatet:
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+        }
+    }
+}
