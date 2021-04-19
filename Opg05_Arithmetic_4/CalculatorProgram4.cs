0605e0dc226c861fd57d2d7ab56493018a0837fc
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-04-19 08:11:50 +0200

    Vejl. løsn til opg. 5.4 udbygget med lærer noter

--- Opg05_Arithmetic_4/CalculatorProgram4.cs
+++ Opg05_Arithmetic_4/CalculatorProgram4.cs
@@ -6,6 +6,11 @@ namespace Opg05_Arithmetic_4
     {
         static void Main(string[] args)
         {
+            /* *** LÆRER NOTER: Modulo (%-opratoren) giver resten ved division. F.eks. 10 % 5 = 0, fordi 5 går op i 10. Men 11 % 5 = 1, fordi der er 1 fra det tætteste 5 går op i, nemlig 10, til 11.
+             * Modulo har en række fordelagtige anvendelser. F.eks. findes om et tal x er lige ved testen x % 2 == 0. Altså spørger man her om der ingen rest er ved divisionen x / 2. Hvis der er en
+             * rest, så er x ulige, hvis der ikke er en rest så er x lige. Kun tallet 2 kan anvendes til dette. Op samme måde kan man undersøge om et tal b går op i et tal a ved testen a % b == 0.
+             * Men da operatoren == først bliver introduceret for eleven i en kommende opgave, bør eleven ikke introduceres for dette i denne opgave.*/
+
             Console.WriteLine("Lommeregner v4");
 
             // Indhent første tal fra brugeren og konverter:
