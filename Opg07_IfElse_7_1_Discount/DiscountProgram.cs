a564bc939067fea37c49f4ed5d1a253fd189262d
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-04-22 13:58:53 +0200

    Vejl. løsn. til opg. 07.1 Rabat

--- Opg07_IfElse_7_1_Discount/DiscountProgram.cs
+++ Opg07_IfElse_7_1_Discount/DiscountProgram.cs
@@ -2,11 +2,21 @@
 
 namespace Opg07_IfElse_7_1_Discount
 {
-    class Program
+    class DiscountProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("RABAT PROGRAMMET");
+            Console.WriteLine("Indtast beløbet (kr. og øre)");
+            string priceInput = Console.ReadLine();
+            decimal price = Convert.ToDecimal(priceInput);
+            decimal discount = default;
+            decimal discountPercentage = 0.05m;
+            if(price > 1000m)
+            {
+                discount = price * discountPercentage;
+            }
+            Console.WriteLine($"Din rabat er {discount:c2} på købet til {price:c2}.\nAt betale: {price - discount:c2}");
         }
     }
 }
