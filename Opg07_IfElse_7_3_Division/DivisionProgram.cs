fdb75a1ac47ecd9c150e76b99602f56755f4b784
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-04-22 14:44:59 +0200

    Vejl. løsn. til opg. 07.3 division

---
+++ Opg07_IfElse_7_3_Division/DivisionProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Opg07_IfElse_7_3_Division
+{
+    class DivisionProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIVISIONSPROGRAM");
+            Console.WriteLine("Indtast det første tal:");
+            string firstNumberInput = Console.ReadLine();
+
+            Console.WriteLine("Indtast det andet tal:");
+            string secondNumberInput = Console.ReadLine();
+
+            int firstNumber, secondNumber;
+            firstNumber = Convert.ToInt32(firstNumberInput);
+            secondNumber = Convert.ToInt32(secondNumberInput);
+            
+            if(secondNumber == 0)
+            {
+                Console.WriteLine("Kan ikke dividere med 0");
+            }
+            else
+            {
+                Console.WriteLine($"{firstNumber}/{secondNumber} = {firstNumber/secondNumber}");
+            }
+        }
+    }
+}
