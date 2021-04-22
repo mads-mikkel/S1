a3418b59b5d9e822dca58430a0b741adb6aec659
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-04-22 14:34:56 +0200

    Vejl. løsn. til opg. 07.2 to ens tal

--- Opg07_IfElse_7_1_Discount/DiscountProgram.cs
+++ Opg07_IfElse_7_1_Discount/DiscountProgram.cs
@@ -8,10 +8,16 @@ namespace Opg07_IfElse_7_1_Discount
         {
             Console.WriteLine("RABAT PROGRAMMET");
             Console.WriteLine("Indtast beløbet (kr. og øre)");
+
+            // Hent input og konverter. Anvend decimal da den datatype er designet til penge:
             string priceInput = Console.ReadLine();
             decimal price = Convert.ToDecimal(priceInput);
+
+            // Initialiser to variable, én til rabatbeløbet og én til rabatprocenten:
             decimal discount = default;
             decimal discountPercentage = 0.05m;
+
+            // Brug større end operatoren
             if(price > 1000m)
             {
                 discount = price * discountPercentage;
---
+++ Opg07_IfElse_7_2_TwoEquals/TwoEqualsProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Opg07_IfElse_7_2_TwoEquals
+{
+    class TwoEqualsProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ER DE ENS?");
+            Console.WriteLine("Indtast det første tal:");
+            string firstNumberInput = Console.ReadLine();
+
+            Console.WriteLine("Indtast det andet tal:");
+            string secondNumberInput = Console.ReadLine();
+
+            int firstNumber, secondNumber;
+            firstNumber = Convert.ToInt32(firstNumberInput);
+            secondNumber = Convert.ToInt32(secondNumberInput);
+
+            if(firstNumber == secondNumber)
+            {
+                Console.WriteLine("De to tal er ens.");
+            }
+            else
+            {
+                Console.WriteLine("De to tal er ikke ens.");
+            }
+        }
+    }
+}
