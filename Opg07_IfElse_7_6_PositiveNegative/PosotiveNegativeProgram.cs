819479c0a5e62dd321249ac421dbb30fddc7f6af
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-04-22 15:14:30 +0200

    Vejl. løsn. til opg. 07.6 positivt negativt

---
+++ Opg07_IfElse_7_6_PositiveNegative/PosotiveNegativeProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Opg07_IfElse_7_6_PositiveNegative
+{
+    class PosotiveNegativeProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POSITIVT NEGATIVT PROGRAM");
+            Console.WriteLine("Indtast et tal:");
+            string numberInput = Console.ReadLine();
+
+            int number = Convert.ToInt32(numberInput);
+
+            string output = String.Empty;   // Her anvender jeg en variabel, som jeg bagefter tildeler en værdi i if-else kontrolstrukturen. Først når dette er sket, udskriver jeg variablen.
+            if(number > 0)
+            {
+                output = $"{number} er positivt";
+            }
+            else if(number < 0)
+            {
+                output = $"{number} er negativt";
+            }
+            else    // Igen anvendes udelukkelsesmetoden:
+            {
+                output = "Tallet er 0";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
